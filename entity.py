358a9ffa9ffa22a50849d0f8693cc123a376059a
kylejbrk <github@kyleburke.net>
2021-03-03 15:02:42 -0500

    added scrolls

--- entity.py
+++ entity.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import copy
+import math
 from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING, Union
 
 from render_order import RenderOrder
@@ -62,6 +63,12 @@ class Entity:
         # Move the entity by a given amount
         self.x += dx
         self.y += dy
+
+    def distance(self, x: int, y: int) -> float:
+        '''
+        Return the distance between the current entity and the given (x, y) coordinate.
+        '''
+        return math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
 
     def place(self, x: int, y: int, gamemap: Optional[GameMap] = None) -> None:
         '''Place this entity at a new location. Handles moving across GameMaps.'''
--- entity_factories.py
+++ entity_factories.py
@@ -1,9 +1,10 @@
 from components.ai import HostileEnemy
-from components.consumable import HealingConsumable
+from components import consumable
 from components.fighter import Fighter
 from components.inventory import Inventory
 from entity import Actor, Item
  
+# player
 player = Actor(
     char="@",
     color=(255, 255, 255),
@@ -13,6 +14,7 @@ player = Actor(
     inventory=Inventory(capacity=26)
 )
  
+# enemies
 orc = Actor(
     char="o",
     color=(63, 127, 63),
@@ -30,10 +32,28 @@ troll = Actor(
     inventory=Inventory(capacity=0)
 )
 
+# consumables
+confusion_scroll = Item(
+    char='~',
+    color=(207, 63, 255),
+    name='Confusion Scroll',
+    consumable=consumable.ConfusionConsumable(number_of_turns=10)
+)
+fireball_scroll = Item(
+    char='~',
+    color=(255, 0, 0),
+    name='Fireball Scroll',
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3)
+)
 health_potion = Item(
     char='!',
     color=(127, 0, 255),
     name='Health Potion',
-    consumable=HealingConsumable(amount=4),
+    consumable=consumable.HealingConsumable(amount=4),
 )
-
+lightning_scroll = Item(
+    char='~',
+    color=(255, 255, 0),
+    name='Lightning Scroll',
+    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5)
+)
